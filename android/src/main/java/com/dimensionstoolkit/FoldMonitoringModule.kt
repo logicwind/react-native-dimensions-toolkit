f96dca88f52dfffb1480dff65423d272ac2e9471
farhan-pathan-us <farhan.pathan@logicwind.com>
2023-11-02 15:51:09 +0530

    -Refactor code

--- android/src/main/java/com/dimensionstoolkit/FoldMonitoringModule.kt
+++ android/src/main/java/com/dimensionstoolkit/FoldMonitoringModule.kt
@@ -3,12 +3,12 @@ package com.dimensionstoolkit
 import androidx.lifecycle.*
 import androidx.window.layout.FoldingFeature
 import androidx.window.layout.WindowInfoTracker
+import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.WritableMap
 import com.facebook.react.modules.core.DeviceEventManagerModule
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 class FoldMonitoringModule(private val reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
@@ -18,11 +18,8 @@ class FoldMonitoringModule(private val reactContext: ReactApplicationContext) :
   private val foldingEvent = MutableLiveData<FoldingEvent>()
 
   @ReactMethod
-  suspend fun startFoldingEventMonitoring() {
-    val lifecycleOwner = currentActivity as? LifecycleOwner
-    if (lifecycleOwner == null) {
-      return
-    }
+  fun startFoldingEventMonitoring() {
+    val lifecycleOwner = currentActivity as? LifecycleOwner ?: return
 
     val lifecycleScope = lifecycleOwner.lifecycleScope
 
@@ -34,28 +31,27 @@ class FoldMonitoringModule(private val reactContext: ReactApplicationContext) :
           val event = FoldingEvent(
             displayStatus = if (layoutInfo.displayFeatures.isNotEmpty()) "Multiple displays" else "Single display",
             foldType = layoutInfo.displayFeatures.filterIsInstance<FoldingFeature>()
-              .firstOrNull { it -> it.isTableTop() }?.let { "Table Top" }
+              .firstOrNull { it.isTableTop() }?.let { "Table Top" }
               ?: layoutInfo.displayFeatures.filterIsInstance<FoldingFeature>()
-                .firstOrNull { it -> it.isBookPosture() }?.let { "Book Posture" }
+                .firstOrNull { it.isBookPosture() }?.let { "Book Posture" }
               ?: "Normal Posture"
           )
 
-          // Switch to the main thread before invoking observe
-          withContext(Dispatchers.Main) {
-            foldingEvent.value = event
-          }
+          foldingEvent.postValue(event)
         }
     }
 
-    // Switch to the main thread before invoking observe
-    withContext(Dispatchers.Main) {
-      foldingEvent.observe(lifecycleOwner) { event ->
-        // Handle the event, e.g., emit it to React Native
-        reactContext
-          .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
-          .emit("onFold", event)
-      }
+    foldingEvent.observe(lifecycleOwner) { event ->
+      reactContext
+        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+        .emit("onFold", Arguments.makeNativeMap(event.toWritableMap()))
     }
+  }
+  private fun FoldingEvent.toWritableMap(): WritableMap {
+    val map = Arguments.createMap()
+    map.putString("displayStatus", displayStatus)
+    map.putString("foldType", foldType)
+    return map
   }
 
   override fun getName(): String {
