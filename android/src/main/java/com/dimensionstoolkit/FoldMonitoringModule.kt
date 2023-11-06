1d0f076894a37ca8b6ed6570bf9679cba694607b
farhan-pathan-us <farhan.pathan@logicwind.com>
2023-11-06 15:32:35 +0530

    -removed callback flow

--- android/src/main/java/com/dimensionstoolkit/FoldMonitoringModule.kt
+++ android/src/main/java/com/dimensionstoolkit/FoldMonitoringModule.kt
@@ -1,27 +1,24 @@
 package com.dimensionstoolkit
 
-import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import androidx.window.layout.FoldingFeature
 import androidx.window.layout.WindowInfoTracker
 import com.facebook.react.bridge.*
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 import com.facebook.react.bridge.WritableMap
 import com.facebook.react.bridge.Arguments
-import kotlinx.coroutines.flow.callbackFlow
-
-interface FoldMonitoringCallback {
-  fun onFoldingEvent(event: WritableMap)
-  fun onError(errorCode: String, errorMessage: String, error: Throwable)
-}
 
 class FoldMonitoringModule(private val reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
 
-  data class FoldingEvent(val displayStatus: String, val foldType: String)
-
-  private var foldMonitoringCallback: FoldMonitoringCallback? = null
+  private var foldingEventCallback: ((WritableMap) -> Unit)? = null
+  private var lastFoldEvent: WritableMap? = null
 
   @ReactMethod
   fun startFoldingEventMonitoring(promise: Promise) {
@@ -33,69 +30,41 @@ class FoldMonitoringModule(private val reactContext: ReactApplicationContext) :
 
     val lifecycleScope = lifecycleOwner.lifecycleScope
 
-    val callback = object : FoldMonitoringCallback {
-      override fun onFoldingEvent(event: WritableMap) {
-        promise.resolve(event)
-      }
-
-      override fun onError(errorCode: String, errorMessage: String, error: Throwable) {
-        promise.reject(errorCode, errorMessage, error)
-      }
+    foldingEventCallback = { event ->
+      promise.resolve(event)
     }
-
-    foldMonitoringCallback = callback
 
     lifecycleScope.launchWhenStarted {
       try {
         monitorFoldingEvents(lifecycleOwner)
       } catch (e: Exception) {
-        callback.onError("MONITORING_ERROR", e.message ?: "Unknown error", e)
+        promise.reject("MONITORING_ERROR", e.message, e)
       }
     }
   }
 
   private suspend fun monitorFoldingEvents(lifecycleOwner: LifecycleOwner) {
-    val foldingFeatureFlow = callbackFlow {
-      WindowInfoTracker.getOrCreate(reactContext)
-        .windowLayoutInfo(reactContext)
-        .collect { layoutInfo ->
-          val event = FoldingEvent(
-            displayStatus = if (layoutInfo.displayFeatures.isNotEmpty()) "Multiple displays" else "Single display",
-            foldType = layoutInfo.displayFeatures.filterIsInstance<FoldingFeature>()
-              .firstOrNull { it.isTableTop() }?.let { "Table Top" }
-              ?: layoutInfo.displayFeatures.filterIsInstance<FoldingFeature>()
-                .firstOrNull { it.isBookPosture() }?.let { "Book Posture" }
-              ?: "Normal Posture"
-          )
+    val windowInfoTracker = WindowInfoTracker.getOrCreate(reactContext)
 
-          val eventMap = event.toWritableMap()
-          send(eventMap)
+    val foldingFeatureFlow = windowInfoTracker
+      .windowLayoutInfo(reactContext)
+      .distinctUntilChanged()  // Emit events only when the folding state changes
+      .collect { layoutInfo ->
+        val foldType = layoutInfo.displayFeatures
+          .filterIsInstance<FoldingFeature>()
+          .firstOrNull { it.isTableTop() }?.let { "Table Top" }
+          ?: layoutInfo.displayFeatures.filterIsInstance<FoldingFeature>()
+            .firstOrNull { it.isBookPosture() }?.let { "Book Posture" }
+          ?: "Normal Posture"
+
+        val event = Arguments.createMap()
+        event.putString("foldType", foldType)
+
+        if (lastFoldEvent == null || lastFoldEvent != event) {
+          lastFoldEvent = event
+          foldingEventCallback?.invoke(event)
         }
-    }
-
-    val job = lifecycleOwner.lifecycleScope.launch {
-      try {
-        foldingFeatureFlow.collect { event ->
-          foldMonitoringCallback?.onFoldingEvent(event)
-        }
-      } catch (e: Exception) {
-        foldMonitoringCallback?.onError("MONITORING_ERROR", e.message ?: "Unknown error", e)
       }
-    }
-
-    lifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
-      override fun onDestroy(owner: LifecycleOwner) {
-        job.cancel()
-        foldMonitoringCallback = null
-      }
-    })
-  }
-
-  private fun FoldingEvent.toWritableMap(): WritableMap {
-    val map = Arguments.createMap()
-    map.putString("displayStatus", displayStatus)
-    map.putString("foldType", foldType)
-    return map
   }
 
   override fun getName(): String {
