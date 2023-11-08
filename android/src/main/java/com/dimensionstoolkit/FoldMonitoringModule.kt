ecf8ccf31b482eeb195e22f20896570c7c6a6e83
farhan-pathan-us <farhan.pathan@logicwind.com>
2023-11-08 11:16:20 +0530

    -All working code

--- android/src/main/java/com/dimensionstoolkit/FoldMonitoringModule.kt
+++ android/src/main/java/com/dimensionstoolkit/FoldMonitoringModule.kt
@@ -1,81 +1,101 @@
 package com.dimensionstoolkit
 
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.lifecycleScope
+import android.content.pm.PackageManager
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.modules.core.DeviceEventManagerModule
+import androidx.core.util.Consumer
+import androidx.window.java.layout.WindowInfoTrackerCallbackAdapter
 import androidx.window.layout.FoldingFeature
 import androidx.window.layout.WindowInfoTracker
-import com.facebook.react.bridge.*
-import kotlinx.coroutines.launch
-import com.facebook.react.bridge.Arguments
-import com.facebook.react.modules.core.DeviceEventManagerModule
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.*
+import androidx.window.layout.WindowLayoutInfo
+import com.facebook.react.bridge.Promise
+import java.util.concurrent.Executors
 
-
-class FoldMonitoringModule(private val reactContext: ReactApplicationContext) :
-  ReactContextBaseJavaModule(reactContext) {
-
-  private var isMonitoring = false
-
-  @ReactMethod
-  fun startFoldingEventMonitoring(promise: Promise) {
-    if (isMonitoring) {
-      promise.reject("ALREADY_MONITORING", "Monitoring is already in progress.")
-      return
-    }
-
-    val lifecycleOwner = currentActivity as? LifecycleOwner
-    if (lifecycleOwner == null) {
-      promise.reject("LIFECYCLE_ERROR", "Could not find a valid LifecycleOwner.")
-      return
-    }
-
-    val foldingFeatureFlow: Flow<FoldingFeature> = flow {
-      WindowInfoTracker.getOrCreate(reactContext)
-        .windowLayoutInfo(reactContext)
-        .collect { layoutInfo ->
-          layoutInfo.displayFeatures.filterIsInstance<FoldingFeature>().firstOrNull()?.let {
-            emit(it)
-          }
-        }
-    }
-
-    isMonitoring = true
-    lifecycleOwner.lifecycleScope.launch {
-      foldingFeatureFlow.collect { foldingFeature ->
-        val foldType = when {
-          foldingFeature.isTableTop() -> "Table Top"
-          foldingFeature.isBookPosture() -> "Book Posture"
-          else -> "Normal Posture"
-        }
-        val event = Arguments.createMap()
-        event.putString("foldType", foldType)
-        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
-          .emit("onFold", event)
-      }
-    }
-
-    promise.resolve("Folding event monitoring started.")
-  }
-
-  @ReactMethod
-  fun stopFoldingEventMonitoring(promise: Promise) {
-    if (!isMonitoring) {
-      promise.reject("NOT_MONITORING", "Monitoring is not in progress.")
-      return
-    }
-
-    isMonitoring = false
-    promise.resolve("Folding event monitoring stopped.")
-  }
+class FoldMonitoringModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+  private var windowInfoTracker: WindowInfoTrackerCallbackAdapter = WindowInfoTrackerCallbackAdapter(WindowInfoTracker.getOrCreate(reactContext))
+  private val layoutStateChangeCallback = LayoutStateChangeCallback()
 
   override fun getName(): String {
-    return "FoldMonitoringKit"
+    return "WindowLayoutInfo"
   }
 
-  private fun FoldingFeature.isTableTop(): Boolean =
-    state == FoldingFeature.State.HALF_OPENED && orientation == FoldingFeature.Orientation.HORIZONTAL
+  @ReactMethod
+  fun isFoldSupported(promise: Promise) {
+    val packageManager = reactApplicationContext.packageManager
+    val featureSupported = packageManager.hasSystemFeature(PackageManager.FEATURE_SENSOR_HINGE_ANGLE)
+    promise.resolve(featureSupported)
+  }
 
-  private fun FoldingFeature.isBookPosture(): Boolean =
-    state == FoldingFeature.State.HALF_OPENED && orientation == FoldingFeature.Orientation.VERTICAL
+  @ReactMethod
+  fun startListening() {
+    windowInfoTracker.addWindowLayoutInfoListener(currentActivity!!, Executors.newSingleThreadExecutor(), layoutStateChangeCallback)
+  }
+
+  @ReactMethod
+  fun stopListening() {
+    windowInfoTracker.removeWindowLayoutInfoListener(layoutStateChangeCallback)
+  }
+
+  inner class LayoutStateChangeCallback : Consumer<WindowLayoutInfo> {
+    override fun accept(newLayoutInfo: WindowLayoutInfo) {
+      val event: WritableMap = Arguments.createMap()
+
+      try {
+        val displayFeaturesList = newLayoutInfo.displayFeatures
+
+        if (displayFeaturesList.isNotEmpty()) {
+          val feature = displayFeaturesList[0] // Assuming there's only one feature
+
+          val featureObject = Arguments.createMap()
+
+          if (feature is FoldingFeature) {
+            val foldingFeature = feature as FoldingFeature
+            featureObject.putString("State", foldingFeature.state.toString())
+            featureObject.putString("Orientation", foldingFeature.orientation.toString())
+            featureObject.putBoolean("IsSeparating", foldingFeature.isSeparating)
+            featureObject.putString("OcclusionType", foldingFeature.occlusionType.toString())
+
+            // Parse and include detailed bounds information
+            val bounds = parseBoundsString(foldingFeature.bounds.toString())
+            featureObject.putMap("Bounds", bounds)
+          }
+
+          event.putMap("displayFeatures", featureObject)
+        }
+      } catch (e: Exception) {
+        event.putString("displayFeatures", "Error parsing displayFeatures")
+      }
+
+      sendEvent(reactApplicationContext, "onLayoutInfoChange", event)
+    }
+
+    private fun parseBoundsString(boundsString: String): WritableMap {
+      val bounds = Arguments.createMap()
+      val regex = Regex("Rect\\((\\d+), (\\d+) - (\\d+), (\\d+)\\)")
+      val matchResult = regex.find(boundsString)
+
+      if (matchResult != null && matchResult.groupValues.size == 5) {
+        val left = matchResult.groupValues[1].toInt()
+        val top = matchResult.groupValues[2].toInt()
+        val right = matchResult.groupValues[3].toInt()
+        val bottom = matchResult.groupValues[4].toInt()
+
+        bounds.putInt("left", left)
+        bounds.putInt("top", top)
+        bounds.putInt("right", right)
+        bounds.putInt("bottom", bottom)
+      }
+
+      return bounds
+    }
+  }
+  private fun sendEvent(reactContext: ReactApplicationContext, eventName: String, params: WritableMap) {
+    reactContext
+      .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+      .emit(eventName, params)
+  }
 }
